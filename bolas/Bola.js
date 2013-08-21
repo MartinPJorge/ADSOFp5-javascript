6b4633206f81234d9bf1f43f8442636404cb81d4
Jorge <foxjorge93@gmail.com>
2013-08-21 15:50:26 +0200

    Añadidos los métodos para necesarios para pegar unas bolas junto a otras.

--- bolas/Bola.js
+++ bolas/Bola.js
@@ -2,7 +2,7 @@
  * Constructor de las bolas que representan las tropas.
  *
  * @author Jorge Martin Perez
- * @version 1.0
+ * @version 1.8
  */
 
 
@@ -10,7 +10,7 @@
 
 /**
  * Constructor de las bolas que representan las tropas.
- * @version 1.1
+ * @version 1.8
  *
  * @param cx - coord.x del centro
  * @param cy - coord.y del centro
@@ -62,6 +62,51 @@ function Bola (cx,cy,r,colorDentro,colorBorde,
 	 * @return r - radio de la bola
 	 */
 	this.getRadio = function () { return r; }
+
+
+	/**
+	 * Getter de los bordes.
+	 * @version 1.0
+	 *
+	 * @return bordes - bordes de la bola
+	 */
+	this.getBordes = function () { return bordes; }
+
+
+	/**
+	 * Setter de cx.
+	 * @version 1.0
+	 *
+	 * @param newCX
+	 *
+	 * @return
+	 */
+	this.setCX = function (newCX) { cx = newCX; }
+
+
+	/**
+	 * Setter de cy.
+	 * @version 1.0
+	 *
+	 * @param newCY
+	 *
+	 * @return
+	 */
+	this.setCY = function (newCY) { cy = newCY; }
+
+
+	/**
+	 * Setter de un borde.
+	 * @version 1.0
+	 *
+	 * @param borde - 'arriba'|'abajo'|'izquierda'|'derecha'
+	 * @param array - [ini,end]
+	 *
+	 * @return
+	 */
+	this.setBorde = function (borde,array) {
+		bordes[borde] = array;
+	}
 
 
 	/**
@@ -141,7 +186,7 @@ function Bola (cx,cy,r,colorDentro,colorBorde,
 	/**
 	 * Determina si la bola pasada por arumento se solapa con
 	 * esta.
-	 * @version 1.0
+	 * @version 1.1
 	 *
 	 * @param bola
 	 *
@@ -158,8 +203,136 @@ function Bola (cx,cy,r,colorDentro,colorBorde,
 		var startY2 = bola.getCY()-bola.getRadio()-3;
 		var endY2 = bola.getCY()+bola.getRadio()+3;
 
-		// -----------------------------
-		// --- DECIDIR SI SE SOLAPAN ---
-		// -----------------------------
+		var seSolapan = false;
+		if((endX >= startX2 && endX <= endX2) || (startX >= startX2 && startX <= endX2))
+			if((endY >= startY2 && endY <= endY2) || (startY >= startY2 && startY <= endY2))
+				seSolapan = true;
+
+		if((endX == startX2) || (startX == endX2) || (endY == startY2) || (startY == endY2))
+			seSolapan = false;
+
+		return seSolapan;
+	}
+
+
+	/**
+	 * Determina si la bola esta rodeada.
+	 * @version 1.0
+	 *
+	 * @param bola
+	 *
+	 * @return true|false
+	 */
+	this.estaRodeada = function () {
+		var estaRodeada = false;
+
+		if((bordes.arriba[0] == bordes.arriba[1]) && 
+			(bordes.derecha[0] == bordes.derecha[1]) && 
+			(bordes.abajo[0] == bordes.abajo[1]) && 
+			(bordes.izquierda[0] == bordes.izquierda[1]))
+			estaRodeada = true;
+
+		return estaRodeada;
+	}
+
+
+	/**
+	 * Determina si se puede pegar la bola pasada por argumento.
+	 * @version 1.0
+	 *
+	 * @param bola
+	 *
+	 * @return true|false
+	 */
+	this.sePuedePegar = function (bola) {
+		var bordes = bola.getBordes();
+		var lado = 2 * (bola.getRadio() + 3);
+		var sePuedePegar = false;
+
+		for(var key in bordes) {
+			sePuedePegar = sePuedePegar ||
+			(lado <= (bordes[key][1] - bordes[key][0]));
+		}
+
+		return sePuedePegar;
+	}
+
+
+	/**
+	 * Pega una bola junto a otra.
+	 * @version 1.0
+	 *
+	 * @param bola
+	 *
+	 * @return
+	 */
+	this.pegar = function (bola) {
+		var lado = 2 * (bola.getRadio() + 3);
+		
+		for(var key in bordes)
+			if(lado <= (bordes[key][1] - bordes[key][0])) {
+				if(key == 'arriba') {
+					bola.setBorde('abajo', [bordes[key][0],bordes[key][0]]);
+					bola.setCX(bordes[key][0]+3+bola.getRadio());
+					bola.setCY(cy-r-3 -3-bola.getRadio());
+					bordes[key][0] += lado;
+				}
+				else if(key == 'derecha') {
+					bola.setBorde('izquierda', [bordes[key][0],bordes[key][0]]);
+					bola.setCX(cx+r+3 +3+bola.getRadio());
+					bola.setCY(bordes[key][0]+3+bola.getRadio());
+					bordes[key][0] += lado;
+				}
+				else if(key == 'abajo') {
+					bola.setBorde('arriba', [bordes[key][0],bordes[key][0]]);
+					bola.setCX(bordes[key][0]+3+bola.getRadio());
+					bola.setCY(cy+r+3 +3+bola.getRadio());
+					bordes[key][0] += lado;
+				}
+				else { //'izquierda'
+					bola.setBorde('derecha', [bordes[key][0],bordes[key][0]]);
+					bola.setCX(cx-r-3 -3-bola.getRadio());
+					bola.setCY(bordes[key][0]+3+bola.getRadio());
+					bordes[key][0] += lado;
+				}
+
+				break;
+			}
+	}
+
+
+	/**
+	 * Pega la bola pasada por argumento a la del ambito, pero
+	 * sin que esta ultima actualice su entorno ocupado.
+	 * @version 1.0
+	 *
+	 * @param bola
+	 *
+	 * @return
+	 */
+	this.pegarVirtual = function (bola) {
+		var lado = 2 * (bola.getRadio() + 3);
+		
+		for(var key in bordes)
+			if(lado <= (bordes[key][1] - bordes[key][0])) {
+				if(key == 'arriba') {
+					bola.setCX(bordes[key][0]+3+bola.getRadio());
+					bola.setCY(cy-r-3 -3-bola.getRadio());
+				}
+				else if(key == 'derecha') {
+					bola.setCX(cx+r+3 +3+bola.getRadio());
+					bola.setCY(bordes[key][0]+3+bola.getRadio());
+				}
+				else if(key == 'abajo') {
+					bola.setCX(bordes[key][0]+3+bola.getRadio());
+					bola.setCY(cy+r+3 +3+bola.getRadio());
+				}
+				else { //'izquierda'
+					bola.setCX(cx-r-3 -3-bola.getRadio());
+					bola.setCY(bordes[key][0]+3+bola.getRadio());
+				}
+
+				break;
+			}
 	}
 }
